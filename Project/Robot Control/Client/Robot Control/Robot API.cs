bb2163822930a3fdf63f19237441152e9160d997
Heo Sejin <hsj3925@naver.com>
2023-06-01 14:40:45 +0900

    0601

---
+++ Project/Robot Control/Client/Robot Control/Robot API.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using RestSharp;
+
+public class Robot
+{
+	public Robot()
+	{
+	}
+
+	public void test()
+	{
+		Console.WriteLine("test called");
+		string baseUrl = @"http://localhost:5000/";
+
+		RestClient client = new RestClient(baseUrl);
+		var request = new RestRequest("/test", Method.Get);
+
+		RestResponse response = client.Get(request);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+		{
+			Console.WriteLine(response.Content);
+		}
+	}
+}
