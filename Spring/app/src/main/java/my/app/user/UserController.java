e37551e46657b620540677cabdf64424bb1d23cd
Hash Fractal <hsj3925@naver.com>
2024-01-23 20:36:33 +0900

    Update UserController.java

--- Spring/app/src/main/java/my/app/user/UserController.java
+++ Spring/app/src/main/java/my/app/user/UserController.java
@@ -1,22 +1,20 @@
 package my.app.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
+@RequestMapping("/user")
 public class UserController {
+	@GetMapping()
+	public String root() {
+		return "hello world!";
+	}
 
 	@GetMapping("/test")
 	public UserDTO getTest() {
 		UserDTO user = new UserDTO("test", 2);
 		return user;
 	}
-
-	@GetMapping()
-	public String root() {
-		return "hello world!";
-	}
-	
-	
 }
