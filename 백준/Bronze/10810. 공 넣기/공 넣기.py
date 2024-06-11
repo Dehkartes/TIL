be8b8aa50fac1b25c18fb808c0c078569617894b
Dehkartes <hsj3925@naver.com>
2024-06-11 22:41:50 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+result = [0 for _ in range(n)]
+
+for _ in range(m):
+	i, j, k = map(int, input().split())
+
+	for i in range(i, j + 1):
+		result[i - 1] = k
+
+print(*result)
