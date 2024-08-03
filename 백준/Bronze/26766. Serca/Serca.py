aeb324ea8b41de304d4cc0b9354e0f8c0faf4ed0
Dehkartes <hsj3925@gmail.com>
2024-08-03 18:30:58 +0900

    [Bronze V] Title: Serca, Time: 148 ms, Memory: 110564 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,5 @@
+def printHeart():
+	print(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     ")
+
+for _ in range(int(input())):
+	printHeart()
