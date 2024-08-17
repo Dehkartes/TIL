a6da90be1624555d5c9e8b7705be7e00c44cd7d1
Dehkartes <hsj3925@gmail.com>
2024-08-17 18:31:08 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+match input():
+	case "NLCS":
+		print("North London Collegiate School")
+	case "BHA":
+		print("Branksome Hall Asia")
+	case "KIS":
+		print("Korea International School")
+	case "SJA":
+		print("St. Johnsbury Academy")
