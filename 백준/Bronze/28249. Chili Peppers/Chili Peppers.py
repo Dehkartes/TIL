e8383434e72d8401ec6e1b08bfa60725e84411af
Dehkartes <hsj3925@gmail.com>
2025-08-07 23:55:08 +0900

    [Bronze IV] Title: Chili Peppers, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28249. Chili Peppers/Chili Peppers.py
@@ -0,0 +1,14 @@
+mp = {
+	"Poblano" : 1500,
+	"Mirasol" : 6000,
+	"Serrano" : 15500,
+	"Cayenne" : 40000,
+	"Thai" : 75000,
+	"Habanero" : 125000
+}
+
+sm = 0
+for _ in range(int(input())):
+	sm += mp[input()]
+
+print(sm)
