1400c4a6bb36db8a155ab84417787cb72fd53e92
Dehkartes <hsj3925@gmail.com>
2025-09-27 23:07:23 +0900

    [Bronze IV] Title: Which Alien?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+
+if 3 <= x and y <= 4:
+	print("TroyMartian")
+
+if x <= 6 and 2 <= y:
+	print("VladSaturnian")
+
+if x <= 2 and y <= 3:
+	print("GraemeMercurian")
