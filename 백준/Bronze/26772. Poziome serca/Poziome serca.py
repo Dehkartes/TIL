41303981431bd7e0ba2d2b43bc4399495a8a01db
Dehkartes <hsj3925@gmail.com>
2025-11-18 23:43:34 +0900

    [Bronze IV] Title: Poziome serca, Time: 52 ms, Memory: 34752 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.py
@@ -0,0 +1,21 @@
+l1 = ' @@@   @@@  '
+l2 = '@   @ @   @ '
+l3 = '@    @    @ '
+l4 = '@         @ '
+l5 = ' @       @  '
+l6 = '  @     @   '
+l7 = '   @   @    '
+l8 = '    @ @     '
+l9 = '     @      '
+
+x = int(input())
+
+print(x * l1)
+print(x * l2)
+print(x * l3)
+print(x * l4)
+print(x * l5)
+print(x * l6)
+print(x * l7)
+print(x * l8)
+print(x * l9)
