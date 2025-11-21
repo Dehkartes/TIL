6be9d073e9b73ddfd9b798fd046e79b2f7bb8da2
Dehkartes <hsj3925@gmail.com>
2025-11-21 23:35:09 +0900

    [Bronze IV] Title: Winning Score, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,15 @@
+a3 = int(input()) * 3
+a2 = int(input()) * 2
+a1 = int(input())
+a = a1 + a2 + a3
+b3 = int(input()) * 3
+b2 = int(input()) * 2
+b1 = int(input())
+b = b1 + b2 + b3
+
+if a > b:
+	print('A')
+elif a < b:
+	print('B')
+else:
+	print('T')
