70e945a283dd2bd192e1ad0dde4c4c0aab4bd0b1
Dehkartes <hsj3925@gmail.com>
2025-12-22 23:51:00 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.py
@@ -0,0 +1,12 @@
+x = {
+	"fdsajkl;": "in-out",
+	"jkl;fdsa" : "in-out",
+
+	"asdf;lkj" : "out-in",
+	";lkjasdf" : "out-in",
+
+	"asdfjkl;" : "stairs",
+	";lkjfdsa" : "reverse"
+}
+
+print(x.get(input(), "molu"))
