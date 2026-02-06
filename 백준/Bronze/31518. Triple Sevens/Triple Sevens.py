ba6c335d7acf061c731059ceb3d59f8891f89406
Dehkartes <hsj3925@gmail.com>
2026-02-07 00:38:31 +0900

    [Bronze IV] Title: Triple Sevens, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31518. Triple Sevens/Triple Sevens.py
@@ -0,0 +1,13 @@
+input()
+
+is7 = True
+if not '7' in input().split():
+	is7 = False
+
+if not '7' in input().split():
+	is7 = False
+	
+if not '7' in input().split():
+	is7 = False
+
+print("777" if is7 else "0")
